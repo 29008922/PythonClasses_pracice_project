737a89b5d4c961aa75e011845f2ac856eed38657
HARON <harom.murumba22@gmail.com>
2023-05-14 10:14:40 -0700

    added a new object

--- classes1_project.py
+++ classes1_project.py
@@ -23,12 +23,25 @@ class Teacher:
     def teach(self, experience):
         if experience:
             print("have taught for over 30 year")
-        else:
-            print("I Started teaching few years ago")
+        print("Good work Teacher")
+    print('\n')
 
 
-
-Tr_1 = Teacher("HARON MURUMBA", "HOD", "SCIENCES", "BIO AND CHEM", 5, "lab\n")
+#object1
+Tr_1 = Teacher('HARON MURUMBA', 'HOD', 'SCIENCES', 'CHEMISTRY', 5, 'HODs SC Office')
 print(f"My name is {Tr_1.name} and iam {Tr_1.position} {Tr_1.department} at \
 Gikuni secondary school teachig {Tr_1.subject} for the last \
-{Tr_1.experience} years and my office is in the school {Tr_1.office}")
+{Tr_1.experience} years and my office is {Tr_1.office}")
+print(Tr_1.station)
+print(Tr_1.teach(30))
+print('\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+
+
+#object 2
+Tr_2 = Teacher('MARYANN K', 'DEPUTY PRINCIPAL', 'COMP SCI', 'MATH', 4, 'DPs Office')  
+
+print(f"Her name is {Tr_2.name} and she is {Tr_2.position} at Gikuni secondary\
+ school also teaching {Tr_2.subject} for the last {Tr_2.experience} years")
+
+print(Tr_2.station)
+print(Tr_2.teach(20))
---
+++ roman1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def to_roman(n):
+'''convert integer to Roman numeral'''
+    pass
